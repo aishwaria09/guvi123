7b917040f808af7bac406d1f5a06596964a8a38f
aishwaria09 <54059759+aishwaria09@users.noreply.github.com>
2019-08-14 15:24:40 +0530

    Add files via upload

---
+++ vowel.py
@@ -0,0 +1,8 @@
+a=str(input())
+if(a=="a" or a=="e" or a=="i" or a=="o" or a=="u"):
+    print("Vowel")
+elif(a=="b" or a=="c" or a=="d" or a=="f" or a=="g" or a=="h" or a=="j" or a=="k" or a=="l" or a=="m" or a=="n"  or a=="p" or a=="q" or a=="r" or a=="s" or a=="t" or a=="v " or a=="w" or a=="x" or a=="y" or a=="z"):
+    print("Consonant")
+else:
+    print("invalid")
+    
